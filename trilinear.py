bef49b48b404576b2d6ddae82642fcc33b0d7bd4
zinccat <zincchloride@outlook.com>
2023-05-25 19:37:14 +0800

    trilinear

--- trilinear.py
+++ trilinear.py
@@ -6,65 +6,49 @@ from torch import jit
 
 class Interpol(Function):
     @staticmethod
-    def forward(ctx, grid_values, points, grid_x, grid_y, grid_z):
+    def forward(ctx, grid_values, points, grid):
         # Perform trilinear interpolation
-        x_indices = torch.searchsorted(grid_x, points[0], right=False) - 1
-        y_indices = torch.searchsorted(grid_y, points[1], right=False) - 1
-        z_indices = torch.searchsorted(grid_z, points[2], right=False) - 1
-
-        x_weights = (points[0] - grid_x[x_indices]) / (
-            grid_x[x_indices + 1] - grid_x[x_indices]
-        )
-        y_weights = (points[1] - grid_y[y_indices]) / (
-            grid_y[y_indices + 1] - grid_y[y_indices]
-        )
-        z_weights = (points[2] - grid_z[z_indices]) / (
-            grid_z[z_indices + 1] - grid_z[z_indices]
-        )
+        n_dim = points.shape[0]
+        indices = torch.zeros_like(points, dtype=torch.long)
+        weights = torch.zeros_like(points)
+        for i in range(n_dim):
+            indices[i] = torch.searchsorted(grid[i], points[i], right=False) - 1
+        for i in range(n_dim):
+            weights[i] = (points[i] - grid[i][indices[i]]) / (
+                grid[i][indices[i] + 1] - grid[i][indices[i]]
+            )
         batch_indices = torch.arange(grid_values.shape[0], device=grid_values.device)
-        c000 = grid_values[batch_indices, x_indices, y_indices, z_indices]
-        c001 = grid_values[batch_indices, x_indices, y_indices, z_indices + 1]
-        c010 = grid_values[batch_indices, x_indices, y_indices + 1, z_indices]
-        c011 = grid_values[batch_indices, x_indices, y_indices + 1, z_indices + 1]
-        c100 = grid_values[batch_indices, x_indices + 1, y_indices, z_indices]
-        c101 = grid_values[batch_indices, x_indices + 1, y_indices, z_indices + 1]
-        c110 = grid_values[batch_indices, x_indices + 1, y_indices + 1, z_indices]
-        c111 = grid_values[batch_indices, x_indices + 1, y_indices + 1, z_indices + 1]
 
-        result = (
-            c000 * (1 - x_weights) * (1 - y_weights) * (1 - z_weights)
-            + c001 * (1 - x_weights) * (1 - y_weights) * z_weights
-            + c010 * (1 - x_weights) * y_weights * (1 - z_weights)
-            + c011 * (1 - x_weights) * y_weights * z_weights
-            + c100 * x_weights * (1 - y_weights) * (1 - z_weights)
-            + c101 * x_weights * (1 - y_weights) * z_weights
-            + c110 * x_weights * y_weights * (1 - z_weights)
-            + c111 * x_weights * y_weights * z_weights
-        )
-
+        pn_dim = 2**n_dim
+        c = torch.zeros((pn_dim, points.shape[1]), device=points.device)
+        for i in range(pn_dim):
+            dx = i // 4
+            dy = (i % 4) // 2
+            dz = i % 2
+            c[i] = grid_values[
+                batch_indices, indices[0] + dx, indices[1] + dy, indices[2] + dz
+            ]
+        result = torch.zeros((points.shape[1],), device=points.device)
+        for i in range(pn_dim):
+            result += c[i] * (
+                (1 - weights[0] if i // 4 == 0 else weights[0])
+                * (1 - weights[1] if (i % 4) // 2 == 0 else weights[1])
+                * (1 - weights[2] if i % 2 == 0 else weights[2])
+            )
         ctx.save_for_backward(
             grid_values,
-            x_indices,
-            y_indices,
-            z_indices,
-            x_weights,
-            y_weights,
-            z_weights,
+            indices,
+            weights,
         )
-
         return result
 
     @staticmethod
-    @jit.script
+    # @jit.script
     def backward_cuda(
         grad_output,
         grid_values,
-        x_indices,
-        y_indices,
-        z_indices,
-        x_weights,
-        y_weights,
-        z_weights,
+        indices,
+        weights,
     ):
         batch_indices = torch.arange(grid_values.shape[0], device=grid_values.device)
         grad_input = torch.zeros_like(grid_values)
@@ -73,40 +57,25 @@ class Interpol(Function):
             dy = (i // 2) % 2
             dz = i % 2
             weight = (
-                (1 - x_weights if dx == 0 else x_weights)
-                * (1 - y_weights if dy == 0 else y_weights)
-                * (1 - z_weights if dz == 0 else z_weights)
+                (1 - weights[0] if dx == 0 else weights[0])
+                * (1 - weights[1] if dy == 0 else weights[1])
+                * (1 - weights[2] if dz == 0 else weights[2])
             )
-
-            grad_weight = grad_output * weight
-
             grad_input[
-                batch_indices, x_indices + dx, y_indices + dy, z_indices + dz
-            ] += grad_weight
+                batch_indices,
+                indices[0] + dx,
+                indices[1] + dy,
+                indices[2] + dz,
+            ] += (
+                grad_output * weight
+            )
 
         return grad_input
 
     @staticmethod
     def backward(ctx, grad_output):
-        (
-            grid_values,
-            x_indices,
-            y_indices,
-            z_indices,
-            x_weights,
-            y_weights,
-            z_weights,
-        ) = ctx.saved_tensors
-        grad_values = Interpol.backward_cuda(
-            grad_output,
-            grid_values,
-            x_indices,
-            y_indices,
-            z_indices,
-            x_weights,
-            y_weights,
-            z_weights,
-        )
+        grid_values, indices, weights = ctx.saved_tensors
+        grad_values = Interpol.backward_cuda(grad_output, grid_values, indices, weights)
         return (
             grad_values,
             None,
@@ -121,6 +90,7 @@ if __name__ == "__main__":
     ypos = np.linspace(0, 4, 3)
     zpos = np.linspace(0, 6, 3)
     np.random.seed(0)
+    torch.random.manual_seed(0)
     values = np.random.rand(3, 3, 3)
     points = torch.rand(3, 400)
     xpos = torch.tensor(xpos, dtype=torch.float32)
@@ -129,18 +99,21 @@ if __name__ == "__main__":
     values = torch.tensor(values, requires_grad=True, dtype=torch.float32).repeat(
         points.shape[1], 1, 1, 1
     )
+    # print(values.shape)
+    # exit(0)
 
     # move all to cuda
     device = "cuda" if torch.cuda.is_available() else "cpu"
     xpos = xpos.to(device)
     ypos = ypos.to(device)
     zpos = zpos.to(device)
+    grid = [xpos, ypos, zpos]
     values = values.to(device)
     points = points.to(device)
 
-    interpolated_values = Interpol.apply(values, points, xpos, ypos, zpos)
+    interpolated_values = Interpol.apply(values, points, grid)
     values.retain_grad()
 
-    print(interpolated_values)
+    print(interpolated_values.shape)
     interpolated_values.sum().backward()
     print(values.grad)
